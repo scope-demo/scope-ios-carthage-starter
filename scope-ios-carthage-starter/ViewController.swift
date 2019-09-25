7deb12808b41b1a6e976d82422b5dc1ef9e3993d
Ignacio Bonafonte <nacho@undefinedlabs.com>
2019-09-25 10:39:20 +0200

    First commit

---
+++ scope-ios-carthage-starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scope-ios-carthage-starter
+//
+//  Created by Ignacio Bonafonte on 25/09/2019.
+//  Copyright © 2019 Undefined Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
